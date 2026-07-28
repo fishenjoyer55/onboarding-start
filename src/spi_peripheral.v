b2ac0a58b30c10eef66e8bf351569e915c07a529
Gavin Ruan <gestdec@gmail.com>
2026-07-27 22:58:58 -0400

    oops forgot to track sync_ff

--- src/spi_peripheral.v
+++ src/spi_peripheral.v
@@ -1,17 +1,17 @@
 `default_nettype none
 
 module spi_peripheral (
-    input wire unsynced_SCLK
-    input wire clk
-    input wire reset
-    input wire unsynced_COPI
-    input wire unsynced_nCS
+    input wire unsynced_SCLK,
+    input wire clk,
+    input wire reset,
+    input wire unsynced_COPI,
+    input wire unsynced_nCS,
 
     output reg [7:0] en_reg_out_7_0,
     output reg [7:0] en_reg_out_15_8,
     output reg [7:0] en_reg_pwm_7_0,
     output reg [7:0] en_reg_pwm_15_8,
-    output reg [7:0] pwm_duty_cycle,
+    output reg [7:0] pwm_duty_cycle
 );
 
 wire SCLK, COPI, nCS;
@@ -42,9 +42,9 @@ wire SCLK_posedge, nCS_negedge;
 reg [15:0] COPI_holder;
 reg [4:0] counter;
 
-assign SCLK_posedge = SCLK & ~SCLK_prev; //bit incoming signal
+assign SCLK_posedge = (SCLK & ~SCLK_prev); //bit incoming signal
 //assign nCS_posedge = nCS & ~nCS_prev; //end transaction signal. uh actually this is only true if each transaction is one message. not sure about this one
-assign nCS_negedge = ~nCS & nCS_prev; //start transaction signal
+assign nCS_negedge = (~nCS & nCS_prev); //start transaction signal
 
 always @(posedge clk, negedge reset_n) begin
     
@@ -77,19 +77,19 @@ always @(posedge clk, negedge reset_n) begin
             //writing function only. indicated by COPI_holder[0] == 1
             else if (counter == 4'b1111 && COPI_holder[0] == 1) begin
                 case (COPI_holder[14:8])
-                    7'b0:
+                    7'b000:
                     en_reg_out_7_0 <= COPI_holder[7:0];
 
-                    7'b1:
+                    7'b001:
                     en_reg_out_15_8 <= COPI_holder[7:0];
 
-                    7'b2:
+                    7'b010:
                     en_reg_pwm_7_0 <= COPI_holder[7:0];
 
-                    7'b3:
+                    7'b011:
                     en_reg_pwm_15_8 <= COPI_holder[7:0];
 
-                    7'b4:
+                    7'b100:
                     pwm_duty_cycle <= COPI_holder[7:0];
                 endcase
 
