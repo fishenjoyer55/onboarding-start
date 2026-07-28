b462190fe8da33b7d41dbbc17c2478cd2ed9a61f
Gavin Ruan <gestdec@gmail.com>
2026-07-27 22:28:00 -0400

    spi peripheral complete hopefully

--- src/spi_peripheral.v
+++ src/spi_peripheral.v
@@ -23,21 +23,81 @@ sync_ff SCLK_syncer (
     .clk(clk),
     .reset_n(reset_n),
     .out(SCLK)
-)
+);
 
 sync_ff COPI_syncer (
     .in(unsynced_COPI),
     .clk(clk),
     .reset_n(reset_n),
     .out(COPI)
-)
+);
 
 sync_ff nCS_syncer (
     .in(unsynced_nCS),
     .clk(clk),
     .reset_n(reset_n),
     .out(nCS)
-)
+);
 
+reg SCLK_prev;
+reg nCS_prev;
+wire SCLK_posedge, nCS_posedge, nCS_negedge;
+reg [15:0] COPI_holder;
+reg [4:0] counter;
+
+assign SCLK_posedge = SCLK & ~SCLK_prev; //bit incoming signal
+assign nCS_posedge = nCS & ~nCS_prev; //end transaction signal
+assign nCS_negedge = ~nCS & nCS_prev; //start transaction signal
+
+always @(posedge clk, negedge reset_n) begin
+    
+    if (~reset_n) begin
+        en_reg_out_15_8 <= 8'b0;
+        en_reg_out_7_0 <= 8'b0;
+        en_reg_pwm_15_8 <= 8'b0;
+        en_reg_pwm_7_0 <= 8'b0;
+        pwm_duty_cycle <= 8'b0;
+        SCLK_prev <= 1'b0;
+        nCS_prev <= 1'b0;
+        COPI_holder <= 16'b0;
+        counter <= 4'b0;
+    end
+
+    else begin
+
+        if (nCS_negedge) begin
+            COPI_holder <= 16'b0;
+            counter <= 4'b0;
+        end
+        else if (~nCS && SCLK_posedge) begin
+
+
+            if (counter < 4'b16) begin
+                COPI_holder <= {COPI_holder[14:0], COPI[0]};
+                counter <= counter + 1;
+            end
+
+            //writing function only. indicated by COPI_holder[0] == 1
+            else if (counter == 4'b16 && COPI_holder[0] == 1) begin
+                case (COPI_holder[14:8])
+                    7'b0:
+                    en_reg_out_7_0 <= COPI_holder[7:0];
+
+                    7'b1:
+                    en_reg_out_15_8 <= COPI_holder[7:0];
+
+                    7'b2:
+                    en_reg_pwm_7_0 <= COPI_holder[7:0];
+
+                    7'b3:
+                    en_reg_pwm_15_8 <= COPI_holder[7:0];
+                endcase
+             end
+        end 
+
+        SCLK_prev <= SCLK;
+        nCS_prev <= nCS;
+    end
+end
 
 endmodule
