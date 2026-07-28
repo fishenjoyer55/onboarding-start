fde4e2c96a758cf39f93ebf6261bae97f1079138
Gavin Ruan <gestdec@gmail.com>
2026-07-27 23:16:32 -0400

    bro i missed one comma

--- src/project.v
+++ src/project.v
@@ -53,7 +53,7 @@ module tt_um_uwasic_onboarding_gavin_ruan (
     .en_reg_out_15_8(en_reg_out_15_8),
     .en_reg_pwm_7_0(en_reg_pwm_7_0),
     .en_reg_pwm_15_8(en_reg_pwm_15_8),
-    .pwm_duty_cycle(pwm_duty_cycle),
+    .pwm_duty_cycle(pwm_duty_cycle)
   );
 
 
